43ade1bcf9353d7543831fcd3f4721d57fb17c53
themackenzie <153692507+themackenzie@users.noreply.github.com>
2023-12-12 20:02:47 -0500

    Add files via upload

---
+++ Ejercicios de Laboratorio II/ex02/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    cout << "----------------------------------------------------" << endl;
+    cout << "Codigo de estudiante: 236177" << endl;
+    cout << "Nombres y Apellidos: Anderson Leonardo Flores Macedo" << endl;
+    cout << "----------------------------------------------------" << endl;
+
+
+    int r0 = 99;
+
+    while ( r0 < 199 ){
+        r0 ++;
+        cout << "Number: " << r0 << endl;
+
+    }
+
+    return 0;
+}
